e57e252e4f4976425eccdfb071dca22f6d0bdfa1
TatianaLoskutova <tanya.loskutova333@gmail.com>
2023-05-26 01:27:51 +0300

    post in progress

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express, {Request, Response} from 'express'
-const app = express()
-const port = 3000
+import bodyParser from 'body-parser';
+import {videoRouters} from './router/video-routers';
 
-app.get('/', (req: Request , res: Response) => {
-    res.send('Hello World!')
-})
+// create express app
+const app = express()
+const port = process.env.PORT || 3000
+
+// create middleware
+const parserMiddleware = bodyParser.json()
+app.use(parserMiddleware)
+
+// app routers
+app.use('/videos', videoRouters)
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
