69c26f8bc5b4c08d782d290e97b56d7fb20c7167
TatianaLoskutova <tanya.loskutova333@gmail.com>
2023-05-29 03:56:16 +0300

    get test version 2.5

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ interface Video {
 }
 
 
-const video= [
+let video= [
     {
     id : 0,
     title: 'Test video',
@@ -57,7 +57,7 @@ app.get('/videos/:id', (req: Request, res: Response) => {
         res.sendStatus(404)
     }
 })
-
+// нужно ли чтобы все ошибки
 app.post('/videos', (req: Request, res: Response) => {
     let title = req.body.title
     if (!title || typeof title !== 'string' || !title.trim() || title.length > 40) {
@@ -100,98 +100,28 @@ app.post('/videos', (req: Request, res: Response) => {
         publicationDate: addDays(new Date(), 1).toISOString(),
         availableResolutions: [req.body.availableResolutions.toString()]
     };
-
+    video.push(newVideo)
     res.status(201).send(newVideo)
 
 });
 
+app.delete('/videos/:id', (req:Request, res:Response) => {
+    const id = +req.params.id
+    const savedVideo = video.filter(v => v.id !== id);
+    if (savedVideo.length < video.length) {
+        video = savedVideo
+        res.sendStatus(204)
+    } else {
+        res.sendStatus(404)
+    }
+})
 
-
+app.delete('/testing', (req:Request, res:Response) => {
+    video = [];
+    res.sendStatus(204)
+})
 // Version 1
-// export type ValidationError = {message: string, field: string}
-// function addDays(date: Date, days: number) {
-//     const result = new Date();
-//     result.setDate(result.getDate() + days);
-//     return result
-// }
-// let createdAt = new Date()
-// let videos= [{
-//     id : +new Date(),
-//     title: 'Test video',
-//     author: 'Tatiana',
-//     canBeDownloaded: false,
-//     minAgeRestriction: null,
-//     createdAt: new Date().toISOString(),
-//     publicationDate: addDays(createdAt, 1).toISOString(),
-//     availableResolutions: ['P144']
-// }]
-//
-//
-//
-// app.get('/videos', (req: Request, res: Response) => {
-//     res.status(200).send(videos)
-// })
-// app.get('/videos/:id', (req: Request, res: Response) => {
-//     const foundVideo = videos.find(v => v.id === +req.params.id);
-//
-//     if (!foundVideo) {
-//         res.sendStatus(404)
-//         return;
-//     }
-//
-//     res.status(200).send(foundVideo)
-// })
-//
-// app.post('/videos', (req: Request, res: Response) => {
-//     const errors: ValidationError[] = []
-//     let title = req.body.title
-//     if (!title || typeof title !== 'string'|| !title.trim() || title.length > 40) {
-//         res.status(400).send({
-//             errorsMessages: [{
-//                     message: 'incorrect title',
-//                     filed: 'title'
-//                 }]
-//         })
-//         return;
-//     }
-//     let author = req.body.author
-//     if (!author || typeof author !== 'string' || !author.trim() || author.length > 20) {
-//         res.status(400).send({
-//             errorsMessages: [{
-//                 message: 'incorrect author',
-//                 filed: 'author'
-//             }]
-//         })
-//         return;
-//     }
-//     let resolutions = [ 'P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1440', 'P2160' ];
-//     let availableResolutions = req.body.availableResolutions
-//     if (!availableResolutions || typeof availableResolutions !== 'string' ||
-//         !availableResolutions.trim() || availableResolutions !== resolutions.find(r => r.indexOf(availableResolutions as string) > -1)) {
-//         res.status(400).send({
-//             errorsMessages: [{
-//                 message: 'incorrect availableResolutions',
-//                 filed: 'availableResolutions'
-//             }]
-//         })
-//         return;
-//     }
-//     let newVideo = {
-//         id : +new Date(),
-//         title: title,
-//         author: author,
-//         canBeDownloaded: false,
-//         minAgeRestriction: null,
-//         createdAt: new Date().toISOString(),
-//         publicationDate: addDays(createdAt, 1).toISOString(),
-//         availableResolutions: req.body.availableResolutions
-//     }
-//     videos.push(newVideo);
-//
-//     res.status(201).send(newVideo)
-//
-// });
-//
+
 // app.put('/videos/:id', (req:Request, res:Response) => {
 //     let title = req.body.title;
 //     if (!title || typeof title !== 'string'|| !title.trim() || title.length > 40) {
